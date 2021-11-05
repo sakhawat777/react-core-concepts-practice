cebbd4afb391bb74292e0dad28103efdebb6c0c1
sakhawat777 <biplobbms@gmail.com>
2021-11-05 12:46:13 +0600

    Practice Done

--- src/App.js
+++ src/App.js
@@ -2,24 +2,74 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const nayoks = ['Salman Khan', 'Sakib Khan', 'ShahRukh Khan'];
+	const products = [
+		{ name: 'Photoshop', price: '$79.99' },
+		{ name: 'Illustrator', price: '$69.99' },
+		{ name: 'Premiere Pro', price: '$89.99' },
+	];
+	// const productNames = products.map((product) => product);
+	// console.log(productNames);
+
+	// const nayokNames = nayoks.map((nayok) => nayok);
+	// console.log(nayokNames);
+	return (
+		<div className='App'>
+			<ul>
+				{nayoks.map((nayok) => (
+					<li>{nayok}</li>
+				))}
+				,
+				{products.map((product) => (
+					<li>{product.name}</li>
+				))}
+			</ul>
+			{/* {products.map((pd) => (
+				<Product product={pd}></Product>
+			))} */}
+
+			<Product name={products[0].name} price={products[0].price}></Product>
+			<Product name={products[1].name} price={products[1].price}></Product>
+			<Product name={products[2].name} price={products[2].price}></Product>
+			<Person name='Tina Khan' age='20'></Person>
+			<Person name='Raja Khan' age='22'></Person>
+			<Person name='Sakib Khan' age='24'></Person>
+			<Person name='Mina Khan' age='22'></Person>
+		</div>
+	);
+}
+function Product(props) {
+	// deStructuring
+	// const { name, price } = props;
+	// console.log(name, price);
+	return (
+		<div
+			style={{
+				border: '2px solid gray',
+				borderRadius: '5px',
+				backgroundColor: 'lightgray',
+				height: ' 200px',
+				width: '200px',
+				// float: 'left',
+			}}>
+			<h3>{props.name}</h3>
+			<h5>{props.price}</h5>
+			<button>BUY NOW</button>
+		</div>
+	);
+}
+
+function Person(props) {
+	const personStyle = {
+		border: '2px solid goldenrod',
+		margin: '10px',
+	};
+	return (
+		<div style={personStyle}>
+			<h1>{props.name}</h1>
+			<h3>Hero of the Year age is {props.age} </h3>
+		</div>
+	);
 }
 
 export default App;
